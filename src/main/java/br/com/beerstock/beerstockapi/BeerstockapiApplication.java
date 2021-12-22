a9fbaecd4bb1bf0f3d7f83d95d361d45013679a6
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2021-12-22 16:36:29 -0300

    First commit

---
+++ src/main/java/br/com/beerstock/beerstockapi/BeerstockapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.beerstock.beerstockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerstockapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerstockapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/beerstock/beerstockapi/BeerstockapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.beerstock.beerstockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerstockapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
